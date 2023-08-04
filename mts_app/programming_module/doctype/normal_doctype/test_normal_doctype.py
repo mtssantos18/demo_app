92077703740f145de8a5fc1d9c9a045016f5f013
mtssantos18 <eumtssantos@outlook.com>
2023-08-04 12:47:18 -0300

    my first app pushed

---
+++ mts_app/programming_module/doctype/normal_doctype/test_normal_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Matheus Santos and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestNormalDoctype(FrappeTestCase):
+	pass
---
+++ mts_app/programming_module/doctype/tree_doctype/tree_doctype.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Matheus Santos and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class TreeDoctype(NestedSet):
+	pass
